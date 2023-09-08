0b7b48c9f334884c4f5948bdfd142bfb6faf822d
will <will@gmail.com>
2023-09-08 20:35:35 +0800

    moved and deleted tutorial for CMake

--- 01_hello_world/01_hello_word.cpp
+++ 01_hello_world/01_hello_word.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 int main()
 {
-	std::cout << "hello,world!" << std::endl;
-
+	int currVal = 0, val = 0;
+	if (cin >> currVal)
+	{
+		int cnt = 1;
+		while (cin >> val)
+		{
+			if (val == currVal)
+				++cnt;
+			else
+			{
+				cout << currVal << " occurs " << cnt << " times" << endl;
+				currVal = val;
+				cnt = 1;
+			}
+		}
+		cout << currVal << " occurs " << cnt << " times" << endl;
+	}
 	return 0;
 }
--- c++_notes.md
+++
@@ -1,8 +0,0 @@
-g++ main.cpp -o project
-./project //运行CPP文件生成的二进制文件
-
-#递归思想
-#数据抽象
-#算法分析
-
-
